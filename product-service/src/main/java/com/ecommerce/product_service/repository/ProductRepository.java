4268c50dd8b327681e6f761d9ab20a4a02250d94
skyeyestang99 <tommytang9958@gmail.com>
2024-07-13 16:14:31 -0700

    changed directory

--- product-service/src/main/java/com/ecommerce/product_service/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.product_service.repository;
-
-import com.ecommerce.product_service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends MongoRepository<Product,String>{
-}
