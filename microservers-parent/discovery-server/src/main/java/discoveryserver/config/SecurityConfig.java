50926e5194a7e9f6141ffc12bbd0fed52a79aed1
skyeyestang99 <tommytang9958@gmail.com>
2024-07-23 00:19:05 -0700

    added circuit breaker

---
+++ microservers-parent/discovery-server/src/main/java/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package discoveryserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+        return httpSecurity.build();
+    }
+}
--- microservers-parent/order-service/src/main/java/com/productRepository/order_service/controller/OrderController.java
+++ microservers-parent/order-service/src/main/java/com/productRepository/order_service/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.productRepository.order_service.controller;
 
 import com.productRepository.order_service.dto.OrderRequest;
 import com.productRepository.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +19,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time!";
     }
 }
