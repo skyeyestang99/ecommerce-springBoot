14922407ed03a96b98d4909b29cfb4506fb3879d
ytg6 <Yizhou.TANG.intern@3ds.com>
2024-09-24 10:35:43 -0700

    UPDATE - added circuilt breaker

--- microservers-parent/api-gateway/src/main/java/com/ecommerce/api_gateway/routes/Routes.java
+++ microservers-parent/api-gateway/src/main/java/com/ecommerce/api_gateway/routes/Routes.java
@@ -4,9 +4,13 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+
 
 @Configuration
 public class Routes {
@@ -29,6 +33,14 @@ public class Routes {
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory-service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
                 .build();
     }
 
