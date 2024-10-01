29b2163e6713201a49f8da296fb672a24879db1a
skyeyestang99 <tommytang9958@gmail.com>
2024-09-30 23:41:54 -0700

    UPDATE-inventory rds db setup, dockerfile correctly revised

--- microservers-parent/inventory-service/src/main/java/com/ecommerce/invertory_service/InventoryServiceApplication.java
+++ microservers-parent/inventory-service/src/main/java/com/ecommerce/invertory_service/InventoryServiceApplication.java
@@ -2,10 +2,8 @@ package com.ecommerce.invertory_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
